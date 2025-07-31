bfce0719e4303842b28125cfaf01203c1862a650
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2025-07-31 21:47:00 +0530

    location path changed

--- frontend/src/Components/Shared/Navbar.jsx
+++ frontend/src/Components/Shared/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../public/images/logo-peelz.png";
+const logo = "/images/logo-peelz.png";
 
 const Nav = () => {
   const [menu, setMenu] = useState("home");
